3267ad45034a55c65d3648ff96cf3190f688b45e
jamie baxendale <jrbaxendale@live.com>
2021-11-21 03:42:16 +0000

    saved

--- Assets/Scripts/CBeditBtn.cs
+++ Assets/Scripts/CBeditBtn.cs
@@ -8,35 +8,53 @@ public class CBeditBtn : MonoBehaviour
 {
     public GameObject EditCBpanel;
     public bool Bool;
+    public GameObject MainCanvas;
 
-
-    public void MakeManualCBpanel()
-
+    public void Awake()
     {
-       if (Bool == false)
-       {
-           GameObject CBpanel = Instantiate(EditCBpanel);
-           Bool = true;
-       }
-
-       else if (Bool)
-
-       {
-            GameObject cbPanel = GameObject.Find("CBpanel(Clone)");
-            Destroy(cbPanel);
-            Bool = false;
-       }
+        Bool = false;
+        MainCanvas = GameObject.Find("MainCanvas").gameObject;
 
 
     }
 
 
 
+
+
+
+
+    public void MakeManualCBpanel()
+
+    {
+        
+            Debug.Log("Creating EditPanelCB");
+            GameObject CBpanel = 
+            Instantiate(EditCBpanel, MainCanvas.transform, false);
+            CBpanel.transform.SetParent(MainCanvas.transform);
+
+    }
+
+
+
+    public void CloseCBpanel() 
+
+
+        {
+            Destroy(gameObject.transform.parent.transform.gameObject);
+
+        }
+
+
+    
+
+
+
     public void ManualFSinput()
     {
         
-        TMP_InputField ManualCBinput = gameObject.transform.GetChild(3).transform.GetChild(12).gameObject
-            .GetComponent<TMP_InputField>();
+       // TMP_InputField ManualCBinput = GameObject.FindWithTag("SelectedPanel").gameObject.transform.GetChild(3).transform.GetChild(12).gameObject
+         //   .GetComponent<TMP_InputField>(); ..... THIS IS WRONG and should be the new CBedit Panel...
 
         var CBText = ManualCBinput.GetComponent<TMP_InputField>().text;
 
